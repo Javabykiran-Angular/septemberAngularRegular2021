caafc421148f9e9b4a9d95821047ef49924d2f32
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-09-15 10:05:49 +0530

    Day2

---
+++ Typescript/Day2/demo1.ts
@@ -0,0 +1,34 @@
+
+// console.log("Hello World....")
+// System.out.println("vhjagdvhg");
+// in core java => int a,b;
+// start with letter after u can use number
+//we can use _ & $ 
+//No any white space 
+// no any default keyword
+var a1;
+var a2,a3;
+var a_1,a$2;
+// data Type
+// number(int,long,double,float,short), string (string,charcter=>""/''),
+//boolean (true/false),any(number,string,boolean),void(netural )
+//misec. null & undefined
+
+var b1:number=10;   //forward declaration & defination
+// console.log('Value of b1 is '+b1);
+
+// console.log(`===== value of b1 is ${b1}`)
+
+var b2:number;
+// console.log(`Value of b2 is ${b2}`);
+
+var b3:any;
+b3=2.5;
+console.log('value of b3 is '+b3);
+b3='Sumit Raokhande';
+console.log("Value of b3 is "+b3);
+b3=true;
+console.log("Value of b3 is "+b3);
+
+
+
