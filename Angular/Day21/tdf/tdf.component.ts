b5325d37a489aebf83f27716484f913bf3973f7c
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-10-28 09:34:55 +0530

    Day21

---
+++ Angular/Day21/tdf/tdf.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tdf',
+  templateUrl: './tdf.component.html',
+  styleUrls: ['./tdf.component.css']
+})
+export class TdfComponent implements OnInit {
+
+  contactMethod:any[]=[
+    {id:1,name:'Email'},
+    {id:2,name:'Phone'},
+    {id:3,name:'Office Phone'}
+  ];
+
+  EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+  + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(f){
+    console.log("---- "+f.value.myusername);
+    console.log('----------- '+f.value.con);
+  }
+
+}
